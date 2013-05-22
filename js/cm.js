226ba95062fb0e40f1e9b469f8eb5ced6aba2fc0
Joseph Wolff <thewolff@jdwolff.com>
2013-05-21 22:23:19 -0400

    added new app namespace & events

---
+++ js/cm.js
@@ -0,0 +1,19 @@
+// Ensure CM Namespace
+if (typeof(CM) == 'undefined') CM = {};
+
+CM = (function(window, document, $, undefined){
+  // Private
+  // Public
+  return {
+    init: function() {
+       console.log('inittown');
+
+    }
+
+  };
+
+
+
+
+
+})(window, document, jQuery);
--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
+$(document).ready(function(){
+	CM.init();
+});
 
